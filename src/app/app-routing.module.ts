866b1c30be6275d07d1ab240257d1f49f1fd2d6e
Pierre <ppierr.urb@gmail.com>
2020-07-17 16:29:10 +0200

    setting up the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { pathToFileURL } from 'url';
+import { ProjectsComponent } from './projects/projects.component';
+import { ServicesComponent } from './services/services.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomepageComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: '', redirectTo: 'homepage', pathMatch: 'full'},
+  {path: '**', redirectTo: 'homepage'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    ProjectsComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
